cef4772d91685c496aa40a19b2e90ea268ea7483
baseenhoorn <baseenhoorn@hotmail.com>
2016-03-10 08:42:45 +0100

    Optimized the demos

--- robot-arm.python/demo.py
+++ robot-arm.python/demo.py
@@ -4,26 +4,53 @@ robot_arm = RobotArm.RobotArm("127.0.0.1", 9876)
 robot_arm.timeout = 10
 robot_arm.speed = 1.0
 
-n = 1
+nr = 1
+nl = 1
+right = True
 
-def scan():
-    global n
+
+def left_or_right(right):
+    if right:
+        for x in range(0,nr):
+            robot_arm.move_right()
+    elif not right:
+        for x in range(0,nl):
+            robot_arm.move_left()
+
+def go_back(right):
+    if right:
+        for x in range(0,nr):
+            robot_arm.move_left()
+    elif not right:
+        for x in range(0,nl):
+            robot_arm.move_right()
+
+def scan(right):
+    global nr
+    global nl
     robot_arm.grab()
-    if robot_arm.scan() == "none":
-        robot_arm.move_right()
-        scan()
-        n += 1
+    if right:
+        if robot_arm.scan() == "none":
+            robot_arm.move_right()
+            scan(right)
+            nr += 1
+    elif not right:
+        if robot_arm.scan() == "none":
+            robot_arm.move_left()
+            scan(right)
+            nl += 1
 
 def get_new_block():
     global n
+    global right
 
-    for x in range(0,n):
-        robot_arm.move_right()
+    left_or_right(right)
 
-    scan()
+    scan(right)
     
-    for x in range(0,n):
-        robot_arm.move_left()
+    go_back(right)
+    
+    right = not right
 
 
 for x in range(0, 100000):
